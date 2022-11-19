bd65d1433d5be97a21df45714fa61cdf34f05be3
ChuHoMan <1075293216@qq.com>
2022-11-19 17:24:04 +0800

    feat: use @chuhoman/eslint-config-vue to lint files

--- build/utils/log.ts
+++ build/utils/log.ts
@@ -1,5 +1,5 @@
-import chalk from "chalk";
+import chalk from 'chalk';
 
-export const error = (...args: any[]) => console.log(chalk.bold.red(args))
-export const success = (...args: any[]) => console.log(chalk.green(args))
-export const pending = (...args: any[]) => console.log(chalk.cyan(args))
+export const error = (...args: any[]) => console.log(chalk.bold.red(args));
+export const success = (...args: any[]) => console.log(chalk.green(args));
+export const pending = (...args: any[]) => console.log(chalk.cyan(args));
--- scripts/generateNewComponent.ts
+++ scripts/generateNewComponent.ts
@@ -1,48 +1,48 @@
-import path from 'path'
-import fse from 'fs-extra'
-// @ts-ignore is ok
-import uppercamelcase from 'uppercamelcase'
-import { error, pending, success } from '../build/utils/log'
+import path from 'path';
+import fse from 'fs-extra';
+// @ts-expect-error is ok
+import uppercamelcase from 'uppercamelcase';
+import { error, pending, success } from '../build/utils/log';
 
 function toKebabCase(str: string) {
-    let kebabCaseName = str.replace(/[A-Z]/g, function (match) {
-        return '-' + match.toLowerCase()
-    })
+  let kebabCaseName = str.replace(/[A-Z]/g, (match) => {
+    return `-${match.toLowerCase()}`;
+  });
 
-    if (kebabCaseName.slice(0, 1) === '-') {
-        kebabCaseName = kebabCaseName.slice(1)
-    }
-    return kebabCaseName
+  if (kebabCaseName.slice(0, 1) === '-')
+    kebabCaseName = kebabCaseName.slice(1);
+
+  return kebabCaseName;
 }
 
-const componentName = process.argv[2] || ''
-const upperComponentName = uppercamelcase(componentName)
-const kebabCaseComponentName = toKebabCase(componentName)
+const componentName = process.argv[2] || '';
+const upperComponentName = uppercamelcase(componentName);
+const kebabCaseComponentName = toKebabCase(componentName);
 
 function init() {
-    if (!componentName) {
-        error('[组件名]必填！！请输入组件名')
-        return
-    }
-    if (/^\d+$/.test(componentName)) {
-        error('请使用非纯数字的字符串作为组件名。')
-        return
-    }
+  if (!componentName) {
+    error('[组件名]必填！！请输入组件名');
+    return;
+  }
+  if (/^\d+$/.test(componentName)) {
+    error('请使用非纯数字的字符串作为组件名。');
+    return;
+  }
 
-    const newComponentPath = path.resolve(
-        __dirname,
-        '../packages/pizza-ui/components',
-        kebabCaseComponentName
-    )
-    if (fse.existsSync(newComponentPath)) {
-        error(`已存在${kebabCaseComponentName}组件`)
-        return
-    }
+  const newComponentPath = path.resolve(
+    __dirname,
+    '../packages/pizza-ui/components',
+    kebabCaseComponentName,
+  );
+  if (fse.existsSync(newComponentPath)) {
+    error(`已存在${kebabCaseComponentName}组件`);
+    return;
+  }
 
-    const genVueTemplate = (newComponentPath: string) => {
-        fse.outputFileSync(
-            path.join(newComponentPath, `src/${kebabCaseComponentName}.vue`),
-            `<template>
+  const genVueTemplate = (newComponentPath: string) => {
+    fse.outputFileSync(
+      path.join(newComponentPath, `src/${kebabCaseComponentName}.vue`),
+      `<template>
   <div>P${upperComponentName}</div>
 </template>
 
@@ -52,14 +52,14 @@ defineOptions({
 })
 
 </script>
-        `
-        )
-    }
+        `,
+    );
+  };
 
-    const genTestTemplate = (newComponentPath: string) => {
-        fse.outputFileSync(
-            path.join(newComponentPath, `__tests__/${kebabCaseComponentName}.spec.ts`),
-            `import { mount } from '@vue/test-utils'
+  const genTestTemplate = (newComponentPath: string) => {
+    fse.outputFileSync(
+      path.join(newComponentPath, `__tests__/${kebabCaseComponentName}.spec.ts`),
+      `import { mount } from '@vue/test-utils'
 import ${upperComponentName} from '../src/${kebabCaseComponentName}.vue'
 
 describe('${kebabCaseComponentName}.vue', () => {
@@ -73,41 +73,40 @@ describe('${kebabCaseComponentName}.vue', () => {
     // Assert
     
   })
-})`
-        )
-    }
+})`,
+    );
+  };
 
-
-    const genExportTemplate = (newComponentPath: string) => {
-        fse.outputFileSync(
-            path.join(newComponentPath, `index.ts`),
-            `import { withInstall } from '@pizza-ui/utils'\n
+  const genExportTemplate = (newComponentPath: string) => {
+    fse.outputFileSync(
+      path.join(newComponentPath, 'index.ts'),
+      `import { withInstall } from '@pizza-ui/utils'\n
 import ${upperComponentName} from './src/${kebabCaseComponentName}.vue'\n
 export const P${upperComponentName} = withInstall(${upperComponentName})\n
-export default P${upperComponentName}\n`
-        )
-    }
+export default P${upperComponentName}\n`,
+    );
+  };
 
-    const genTemplateList = [
-        genVueTemplate,
-        genTestTemplate,
-        genExportTemplate,
-    ]
+  const genTemplateList = [
+    genVueTemplate,
+    genTestTemplate,
+    genExportTemplate,
+  ];
 
-    const generateNewComponent = (componentName: string) => {
-        pending(`Start creating ${componentName} component templates...`)
-        fse.ensureDirSync(newComponentPath)
+  const generateNewComponent = (componentName: string) => {
+    pending(`Start creating ${componentName} component templates...`);
+    fse.ensureDirSync(newComponentPath);
 
-        genTemplateList.forEach((template) => {
-            template(newComponentPath)
-        })
+    genTemplateList.forEach((template) => {
+      template(newComponentPath);
+    });
 
-        success(`create ${componentName} template success!!!`)
-    }
+    success(`create ${componentName} template success!!!`);
+  };
 
-    //生成新组件模板
-    generateNewComponent(componentName)
+  // 生成新组件模板
+  generateNewComponent(componentName);
 }
 
-init()
+init();
 
