bb851c6ebe0be99aeb37b5351131e4cfb2f16169
puppet-666 <1063771217@qq.com>
2023-02-24 15:15:13 +0800

    feat: vscode extension for components intellisense

---
+++ packages/pizza-vscode-extension/src/extension.ts
@@ -0,0 +1,8 @@
+import { type ExtensionContext } from 'vscode';
+import { registerCompletions } from './completions';
+
+export function activate(context: ExtensionContext) {
+  registerCompletions(context);
+}
+
+export function deactivate() {}
