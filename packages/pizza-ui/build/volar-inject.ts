cebcb20aa4053ea95bd9a4cc2314ce38a01ca9ba
puppetk <1063771217@qq.com>
2023-03-18 22:39:04 +0800

    fix: remove

--- packages/pizza-ui/build/volar-inject.ts
+++ packages/pizza-ui/build/volar-inject.ts
@@ -4,7 +4,7 @@ import fse from 'fs-extra';
 const cwd = process.cwd();
 const dic = path.resolve(cwd, './dist/pizza-ui.esm.js');
 const targetDic = path.resolve(cwd, '../site/public/pizza/pizza-ui.esm.js');
-// 同步拷贝
+
 fse.copySync(dic, targetDic, {
   overwrite: true,
 });
