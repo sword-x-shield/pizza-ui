ecfe3c793bcc28971da41a73fe2e79e061f78a08
puppet-666 <45839282+puppet-666@users.noreply.github.com>
2023-03-18 22:40:11 +0800

    fix: build script & assets (#60)

---
+++ packages/pizza-ui/build/volar-inject.ts
@@ -0,0 +1,10 @@
+import path from 'path';
+import fse from 'fs-extra';
+
+const cwd = process.cwd();
+const dic = path.resolve(cwd, './dist/pizza-ui.esm.js');
+const targetDic = path.resolve(cwd, '../site/public/pizza/pizza-ui.esm.js');
+
+fse.copySync(dic, targetDic, {
+  overwrite: true,
+});
