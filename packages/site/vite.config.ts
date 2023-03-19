7da667c186088e98d271de96d35541f27381dea3
puppetk <1063771217@qq.com>
2023-03-19 11:27:27 +0800

    fix(ci): asset base

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -10,6 +10,7 @@ const vuePlugin = createVuePlugin({
 }) as any;
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pizza-ui',
   plugins: [
     pizzaSitePlugin(),
     DefineOptions.vite(),
